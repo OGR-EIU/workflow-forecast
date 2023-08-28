1557ce1fec081049f48229af02c0c01df0c04dc0
Jaromír Beneš <jaromir.benes@gmail.com>
2023-08-28 18:42:44 +0200

    Delete prepare_local_environment_template.py

--- analyst/prepare_local_environment_template.py
+++
@@ -1,9 +0,0 @@
-
-import git
-
-model_repo = "@(model_repo_name)"
-model_repo_commitish = "@(model_repo_commitish)"
-
-model_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{model_repo}.git", f"{model-repo)", branch="main", filter="tree:0", no_checkout=True, )
-model_repo.git.checkout(model_repo_commitish)
-
