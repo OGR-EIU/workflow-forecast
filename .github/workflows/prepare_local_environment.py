9797afc35b710135cf0441228ee06478d63de8a0
Jaromír Beneš <jaromir.benes@gmail.com>
2023-08-30 13:32:24 +0200

    Updated workflow

--- .github/workflows/prepare_local_environment.py
+++ .github/workflows/prepare_local_environment.py
@@ -1,14 +1,17 @@
 
 import git
-
-_WORKFLOW_FORECAST_REPO = "@(workflow_forecast_repo)"
-_WORKFLOW_FORECAST_REPO_SHA = "@(workflow_forecast_repo_sha)"
-
-_MODEL_REPO = "@(model_repo)"
-_MODEL_REPO_SHA = "@(model_repo_sha)"
-
+import json
 
 if __name__ == "__main__":
+
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    _WORKFLOW_FORECAST_REPO = config["workflow_forecast_repo"]
+    _WORKFLOW_FORECAST_REPO_SHA = config["workflow_forecast_repo_SHA"]
+
+    _MODEL_REPO = config["model_repo"]
+    _MODEL_REPO_SHA = config["model_repo_SHA"]
 
     workflow_forecast_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_WORKFLOW_FORECAST_REPO}.git", f"{_WORKFLOW_FORECAST_REPO}", filter="tree:0", no_checkout=True, )
     workflow_forecast_repo.git.checkout(f"{_WORKFLOW_FORECAST_REPO_SHA}")
