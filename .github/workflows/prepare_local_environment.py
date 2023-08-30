875710d90da4b90b3ed767f861f7d96490970901
Jaromír Beneš <jaromir.benes@gmail.com>
2023-08-30 07:42:56 +0200

    Updated workflow

--- .github/workflows/prepare_local_environment.py
+++ .github/workflows/prepare_local_environment.py
@@ -2,7 +2,8 @@
 import git
 
 model_repo = "@(model_repo)"
-model_commitish = "@(model_commitish)"
+model_repo_ref = "@(model_repo_ref)"
 
 model_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{model_repo}.git", f"{model_repo}", filter="tree:0", no_checkout=True, )
-model_repo.git.checkout(f"{model_commitish}")
+model_repo.git.checkout(f"{model_repo_ref}")
+
