460d6f933852769834bdc5d6cc888cc7f6faf65a
Jaromír Beneš <jaromir.benes@gmail.com>
2023-08-30 08:23:34 +0200

    Updated workflow

--- .github/workflows/prepare_local_environment.py
+++ .github/workflows/prepare_local_environment.py
@@ -1,9 +1,18 @@
 
 import git
 
-model_repo = "@(model_repo)"
-model_repo_ref = "@(model_repo_ref)"
+_WORKFLOW_FORECAST_REPO = "@(workflow_forecast_repo)"
+_WORKFLOW_FORECAST_REPO_SHA = "@(workflow_forecast_repo_sha)"
 
-model_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{model_repo}.git", f"{model_repo}", filter="tree:0", no_checkout=True, )
-model_repo.git.checkout(f"{model_repo_ref}")
+_MODEL_REPO = "@(model_repo)"
+_MODEL_REPO_SHA = "@(model_repo_sha)"
+
+
+if __name__ == "__main__":
+
+    workflow_forecast_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_WORKFLOW_FORECAST_REPO}.git", f"{_WORKFLOW_FORECAST_REPO}", filter="tree:0", no_checkout=True, )
+    workflow_forecast_repo.git.checkout(f"{_WORKFLOW_FORECAST_REPO_SHA}")
+
+    model_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_MODEL_REPO}.git", f"{_MODEL_REPO}", filter="tree:0", no_checkout=True, )
+    model_repo.git.checkout(f"{_MODEL_REPO_SHA}")
 
