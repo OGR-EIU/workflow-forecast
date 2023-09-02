bacc86780ebfc6bf00722254099703893681a870
Jaromír Beneš <jaromir.benes@gmail.com>
2023-09-02 21:37:04 +0200

    Updated workflow

---
+++ analyst/startup.m
@@ -0,0 +1,44 @@
+
+restoredefaultpath();
+disp(mfilename("fullpath"));
+disp("Setting up forecast environment...");
+
+echo startup on
+
+addpath(fullfile("..", "iris-toolbox"), "-end");
+addpath(fullfile("..", "toolset"), "-end");
+addpath(fullfile("..", "model-infra"), "-end");
+iris.startup("silent", true);
+
+config = jsondecode(fileread("config.json"));
+
+env_paths = struct();
+
+env_paths.this_dir = fileparts(mfilename("fullpath"));
+
+env_paths.model_dir = fullfile(env_paths.this_dir, "model");
+
+env_paths.input_mapping_path = fullfile(env_paths.model_dir, "mappings", "input-data-mapping.json");
+env_paths.output_mapping_path = fullfile(env_paths.model_dir, "mappings", "output-data-mapping.json");
+
+env_paths.input_data_path = fullfile(env_paths.this_dir, "input-data.json");
+env_paths.output_data_path = fullfile(env_paths.this_dir, "output-data.json");
+
+[model, params] = modeler.create_country_model(env_paths.model_dir);
+
+input_data_mapping = jsondecode(fileread(env_paths.input_mapping_path));
+input_data = jsondecode(fileread(env_paths.input_data_path));
+input_db = protocol_conversion.databank_from_response(input_data, input_data_mapping);
+input_db = modeler.prepare_country_input_data(input_db, params);
+
+dates = modeler.define_forecast_dates(model, input_db);
+
+setappdata(0, "env_paths", env_paths);
+setappdata(0, "input_db", input_db);
+setappdata(0, "model", model);
+setappdata(0, "params", params);
+setappdata(0, "dates", dates);
+setappdata(0, "timestamp", string(config.timestamp));
+
+echo off
+
--- artifact/prepare_local_environment.py
+++ artifact/prepare_local_environment.py
@@ -8,29 +8,52 @@ import requests
 import sys
 
 
-BASE_URL = "https://eiu-dev.ogresearch.com/api"
+DATA_WAREHOUSE_URL = "https://eiu-dev.ogresearch.com/api"
 USERNAME = "test-user"
 PASSWORD = "t3stT#st"
 
 
-def request_data(base_url, username, password, request, ):
-    # authenticate to get a token (JWT)
-    url = f'{base_url}/authenticate'
+def _install_dependency(folder:str, dep: dict, ) -> None:
+    logging.info("Cloning " + folder)
+    shutil.rmtree(folder, ignore_errors=True, )
+    workflow_forecast_repo = git.Repo.clone_from(dep["url"], folder, filter="tree:0", no_checkout=True, )
+    workflow_forecast_repo.git.checkout(dep["commit"])
+
+
+def _request_data(request, base_url, username, password, timestamp, ) -> None:
+    logging.info("Requesting forecast input data")
+    for r in request:
+        r["snapshot_time"] = timestamp
+    #
+    # Authenticate to get a token (JWT)
+    url = f"{base_url}/authenticate"
     payload = {
-        'username': username,
-        'password': password
+        "username": username,
+        "password": password,
     }
-    headers = {'content-type': 'application/json'}
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
-    token = response.json()['id_token']
+    headers = {"content-type": "application/json"}
+    response = requests.post(url, data=json.dumps(payload), headers=headers, )
+    token = response.json()["id_token"]
     response.raise_for_status()
     #
     # Post the data request
-    url = f'{base_url}/data-requests/series/retrieve'
-    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
-    response = requests.post(url, data=json.dumps(request), headers=headers)
+    url = f"{base_url}/data-requests/series/retrieve"
+    headers = {"Authorization": "Bearer " + token, "content-type": "application/json"}
+    response = requests.post(url, data=json.dumps(request), headers=headers, )
+    #
     return response
 
+
+def _postprocess_response(response, response_path, ) -> None:
+    if response.status_code == 200:
+        logging.info("Successfully retrieved forecast input data")
+        with open(response_path, "wt", ) as f:
+            json.dump(response.json(), f, indent=4, )
+    elif response.status_code == 400:
+        logging.critical("Bad request: please check if the requested keys exist")
+    else:
+        print(response.json(), file=sys.stderr)
+    response.raise_for_status()
 
 
 if __name__ == "__main__":
@@ -42,71 +65,18 @@ if __name__ == "__main__":
     with open(config_path, "r") as f:
         config = json.load(f)
 
-    _WORKFLOW_FORECAST_REPO = config["workflow_forecast_repo"]
-    _WORKFLOW_FORECAST_REPO_SHA = config["workflow_forecast_repo_SHA"]
+    for folder, dep in config["dependencies"].items():
+        if folder and dep:
+            _install_dependency(folder, dep, )
 
-    _MODEL_REPO = config["model_repo"]
-    _MODEL_REPO_SHA = config["model_repo_SHA"]
-
-    _MODEL_INFRA_REPO = config["model_infra_repo"]
-    _MODEL_INFRA_REPO_SHA = config["model_infra_repo_SHA"]
-
-    _IRIS_TOOLBOX_REPO = config["iris_toolbox_repo"]
-    _IRIS_TOOLBOX_REPO_SHA = config["iris_toolbox_repo_SHA"]
-
-    _TOOLSET_REPO = config["toolset_repo"]
-    _TOOLSET_REPO_SHA = config["toolset_repo_SHA"]
-
-    _DATA_WAREHOUSE_CLIENT_REPO = config["data_warehouse_client_repo"]
-    _DATA_WAREHOUSE_CLIENT_REPO_SHA = config["data_warehouse_client_repo_SHA"]
-
-    logging.info(f"Cloning {_WORKFLOW_FORECAST_REPO} repo")
-    shutil.rmtree(f"{_WORKFLOW_FORECAST_REPO}", ignore_errors=True, )
-    workflow_forecast_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_WORKFLOW_FORECAST_REPO}.git", f"{_WORKFLOW_FORECAST_REPO}", filter="tree:0", no_checkout=True, )
-    workflow_forecast_repo.git.checkout(f"{_WORKFLOW_FORECAST_REPO_SHA}")
-
-    logging.info(f"Cloning {_MODEL_REPO} repo")
-    shutil.rmtree(f"{_MODEL_REPO}", ignore_errors=True, )
-    model_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_MODEL_REPO}.git", f"{_MODEL_REPO}", filter="tree:0", no_checkout=True, )
-    model_repo.git.checkout(f"{_MODEL_REPO_SHA}")
-
-    logging.info(f"Cloning {_MODEL_INFRA_REPO} repo")
-    shutil.rmtree(f"{_MODEL_INFRA_REPO}", ignore_errors=True, )
-    model_infra_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_MODEL_INFRA_REPO}.git", f"{_MODEL_INFRA_REPO}", filter="tree:0", no_checkout=True, )
-    model_infra_repo.git.checkout(f"{_MODEL_INFRA_REPO_SHA}")
-
-    logging.info(f"Cloning {_TOOLSET_REPO} repo")
-    shutil.rmtree(f"{_TOOLSET_REPO}", ignore_errors=True, )
-    toolset_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_TOOLSET_REPO}.git", f"{_TOOLSET_REPO}", filter="tree:0", no_checkout=True, )
-    toolset_repo.git.checkout(f"{_TOOLSET_REPO_SHA}")
-
-    logging.info(f"Cloning {_IRIS_TOOLBOX_REPO} repo")
-    shutil.rmtree(f"{_IRIS_TOOLBOX_REPO}", ignore_errors=True, )
-    iris_toolbox_repo = git.Repo.clone_from(f"git@github.com:IRIS-Solutions-Team/IRIS-Toolbox.git", f"{_IRIS_TOOLBOX_REPO}", filter="tree:0", no_checkout=True, )
-    iris_toolbox_repo.git.checkout(f"{_IRIS_TOOLBOX_REPO_SHA}")
-
-    logging.info(f"Cloning {_DATA_WAREHOUSE_CLIENT_REPO} repo")
-    shutil.rmtree(f"{_DATA_WAREHOUSE_CLIENT_REPO}", ignore_errors=True, )
-    data_warehouse_client_repo = git.Repo.clone_from(f"git@github.com:OGR-EIU/{_DATA_WAREHOUSE_CLIENT_REPO}.git", f"{_DATA_WAREHOUSE_CLIENT_REPO}", filter="tree:0", no_checkout=True, )
-    data_warehouse_client_repo.git.checkout(f"{_DATA_WAREHOUSE_CLIENT_REPO_SHA}")
-
-    logging.info("Requesting forecast input data")
-    with open(os.path.join(_MODEL_REPO, "requests", "input-data-request.json"), "rt") as f:
+    request_path = os.path.join(this_dir, "model", "requests", "input-data-request.json", )
+    with open(request_path, "r") as f:
         request = json.load(f)
 
-    for i in request:
-        i["snapshot_time"] = config["timestamp"]
-    response = request_data(BASE_URL, USERNAME, PASSWORD, request, )
+    timestamp = config["timestamp"]
 
-    if response.status_code == 200:
-        logging.info("Successfully retrieved forecast input data")
-        print(json.dumps(response.json(), indent=4, ))
-        with open("forecast-input-data.json", 'wt') as f:
-            json.dump(response.json(), f, indent=4, )
-    elif response.status_code == 400:
-        logging.critical("Bad request: please check if the requested keys exist")
-    else:
-        print(response.json(), file=sys.stderr)
-    response.raise_for_status()
+    response = _request_data(request, DATA_WAREHOUSE_URL, USERNAME, PASSWORD, timestamp, )
 
+    response_path = os.path.join(this_dir, "input-data.json", )
+    _postprocess_response(response, response_path, )
 
