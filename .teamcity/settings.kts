e127bed5e4a15c6cee43e0170ef2c7ccf4c78971
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-04 16:39:25 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: parameters of 'Forecast checker' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -56,7 +56,7 @@ object ForecastChecker : BuildType({
         text("workflow.dependencies.model.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.config.timestamp", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("email.recipients", "ngocnam.nguyen@ogresearch.com, jaromir.benes@ogresearch.com, sergey.plotnikov@ogresearch.com", label = "Email recipients", description = "List of notification email recipients", allowEmpty = false)
-        param("email.body", "Dear all, please find intermediate EIU PoC Forecast Report attached. Best regards, Ngoc Nam Nguyen")
+        text("email.body", "Dear all, please find intermediate EIU PoC Forecast Report attached. Best regards, Ngoc Nam Nguyen", label = "Email message", description = "Text of the notification email", allowEmpty = false)
         text("workflow.dependencies.toolset.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
     }
 
