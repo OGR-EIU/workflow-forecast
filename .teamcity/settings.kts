1e53a3cf2d55e2184ea4e4ad6854eac64f97d3a8
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-04 15:26:16 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: VCS roots of 'Forecast merger' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -393,7 +393,6 @@ object ForecastMerger : BuildType({
     name = "Forecast merger"
 
     vcs {
-        root(AbsoluteId("ExampleWorkflows_WorkflowForecastPr"))
         root(DslContext.settingsRoot)
     }
 
