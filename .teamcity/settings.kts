e7cb638ce41e77f037efeb0243e2b37147a3383c
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-04 12:31:13 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: parameters of 'Forecast runner' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -403,7 +403,7 @@ object ForecastRunner : BuildType({
         text("workflow.dependencies.model.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.config.timestamp", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("email.body", "Dear all, please find EIU PoC Forecast Report attached. Best regards, Ngoc Nam Nguyen", label = "Email message", description = "Text of the notification email", allowEmpty = false)
-        param("matlab.code.report", "runner('../post-output.json', 'model', true);")
+        param("matlab.code.report", "runner('../data-warehouse-client/post-output.json', 'model', true);")
         text("workflow.dependencies.data-warehouse-client.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.dependencies.iris-toolbox.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.dependencies.model-infra.commit", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
