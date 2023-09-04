00178166e637ad4634a5203073312be8bb2b6f60
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-04 17:24:30 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: runners of 'Forecast merger' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -449,7 +449,7 @@ object ForecastMerger : BuildType({
                 
                 input_branch_name=%workflow.output.forecast-branch-name%
                 output_branch_name=${'$'}(echo ${'$'}forecast_branch_name | sed 's/-ANALYST//g')
-                export GH_TOKEN=
+                export GITHUB_TOKEN=
                 
                 gh auth login --with-token <<< %env.GH_TOKEN%
                 gh pr create \
