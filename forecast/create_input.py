dddf01a4eb5066d4efb9edcb12fd4b54975902dd
Ngoc Nam Nguyen <nambrno1998@gmail.com>
2023-09-04 14:55:48 +0200

    Removing runner, adjusting create input script

--- forecast/create_input.py
+++ forecast/create_input.py
@@ -3,38 +3,35 @@ import argparse
 from datetime import datetime
 
 
-def process_settings(config_path, output_file, params=None, ):
-
-    params = params or {}
-    
-    # load json file with settings
+def process_settings(config_path, output_file, params={}):
+    # load json file with requests
     with open(config_path) as f:
-        settings = json.load(f)
+        requests = json.load(f)
 
     # go through each item in the request list
-    for i in range(len(settings['json_request'])):
+    for item in requests:
 
         for key in params:
-            settings['json_request'][i][key] = params[key]
+            item[key] = params[key]
 
         # automatic value replacements
-        if 'snapshot_time' in settings['json_request'][i] \
-            and not settings['json_request'][i]['snapshot_time']:
-            settings['json_request'][i]['snapshot_time'] = datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
+        if 'snapshot_time' in item \
+            and not item['snapshot_time']:
+            item['snapshot_time'] = datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
 
-    print(f'Processed forecast report settings:')
-    print(json.dumps(settings, indent=2))
+    print(f'Processed forecast report requests:')
+    print(json.dumps(requests, indent=2))
 
-    # save the processed settings back to settings JSON
+    # save the processed requests back to requests JSON
     with open(output_file, 'w') as f:
-        json.dump(settings, f, indent=2)
+        json.dump(requests, f, indent=2)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config-path', help='path to JSON file with settings', required=True)
-    parser.add_argument('--output-file', help='path to JSON file where the step settings will be saved')
-    parser.add_argument('--params-json', default='{}', help='JSON string with step parameters')
+    parser.add_argument('--config-path', help='path to JSON file with requests', required=True)
+    parser.add_argument('--output-file', help='path to JSON file where the requests will be saved')
+    parser.add_argument('--params-json', default='{}', help='JSON string with parameters')
     args = parser.parse_args()
     if args.output_file is None:
         args.output_file = f'adjusted-{args.config_path}'
