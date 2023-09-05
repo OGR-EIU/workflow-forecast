27d59b3266f1742df37b5719e412bc0471f21e5a
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-05 12:08:27 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: parameters of 'Forecast initializer' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -357,6 +357,7 @@ object ForecastInitializer : BuildType({
         text("workflow.output.timestamp", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("env.IRIS_TOOLBOX_REPO", "iris-toolbox", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.output.rel-config-path", "", display = ParameterDisplay.HIDDEN, allowEmpty = true)
+        param("workflow.dependencies.model-cz.tag", "")
         text("env.CI_AUTHOR", "Production workflow", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("env.TOOLSET_REPO", "toolset", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("workflow.dependencies.model-ea.commit", "HEAD", label = "Model EA commit", description = "Commit id of the Model EA repo", display = ParameterDisplay.PROMPT, allowEmpty = false)
