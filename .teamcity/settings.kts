6970a2bc19213bae555fd442bfa11e27c0064846
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-05 11:06:23 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: VCS roots of 'Forecast comparer' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -227,7 +227,6 @@ object ForecastComparer : BuildType({
         root(AbsoluteId("ExampleWorkflows_Iris"), "+:. => iris-toolbox")
         root(AbsoluteId("ExampleWorkflows_Toolset"), "+:. => toolset")
         root(DslContext.settingsRoot, "+:. => workflow-forecast")
-        root(AbsoluteId("ExampleWorkflows_ModelCz"), "+:. => model-cz")
         root(AbsoluteId("ExampleWorkflows_ModelEa"), "+:. => model-ea")
         root(AbsoluteId("ExampleWorkflows_ModelUs"), "+:. => model-us")
     }
