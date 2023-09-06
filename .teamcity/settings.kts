c47cd92e862d2201268540448132f756e472c479
ngocnam.nguyen <ngocnam.nguyen@TeamCity>
2023-09-06 12:46:12 +0000

    TeamCity change in 'Example workflows / Model forecasts' project: runners of 'Forecast checker' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -198,6 +198,16 @@ object ForecastChecker : BuildType({
                 scriptArguments = "--subject '%email.subject%' --recipients '%email.recipients%' --body '%email.body%' --attachment './report-forecast/results/report-forecast.bundle.html'"
             }
         }
+        python {
+            name = "Check forecast (1)"
+            workingDir = "workflow-forecast"
+            environment = venv {
+            }
+            command = file {
+                filename = "forecast/check_forecast.py"
+                scriptArguments = "--response-path ../output-data.json --mapping-path ../model/input-data-mapping.json"
+            }
+        }
     }
 
     features {
